636be84eb3c3f7fde2e1ba4a11ecb2427c66214f
Poul Sander <poul@poulsander.com>
2026-03-25 18:35:06 +0100

    Restored config samples to working versions

--- config-mysql.js
+++ config-mysql.js
@@ -1,32 +1,32 @@
 {
-  application: {
+  "application" : {
     // user specific data
-    connection_string: "mysql:database=oastat;user=testuser;password='password'",
-    media_path: "/media",
-    static_media: "/static",
+    "connection_string" : "mysql:database=oastat;user=testuser;password='password'",
+    "media_path" : "/media",
+    "static_media" : "/static"
   },
-  service: {
-    api: "http",
-    port: 27960,
+  "service" : {
+    "api" : "http",
+    "port" : 27960
   },
-  http: {
-    script: "/mb.fcgi",
-    rewrite: [
-      { regex: "/media(/.*)?", pattern: "$0" },
-      { regex: "/static(/.*)?", pattern: "$0" },
-      { regex: "/files/(media|static)(/.*)?", pattern: "$0" },
-      { regex: "/favicon\\.ico", pattern: "$0" },
-      { regex: ".*", pattern: "/mb.fcgi$0" },
+  "http" : {
+    "script" : "/mb.fcgi",
+    "rewrite" : [
+      { "regex" : "/media(/.*)?", "pattern" : "$0" },
+      { "regex" : "/static(/.*)?", "pattern" : "$0" },
+      { "regex" : "/files/(media|static)(/.*)?", "pattern" : "$0" },
+      { "regex" : "/favicon\\.ico", "pattern" : "$0" },
+      { "regex" : ".*", "pattern" : "/mb.fcgi$0" }
+    ]
+  },
+  "file_server" : {
+    "enable" : true,
+    "document_root" : ".",
+    "alias" : [
+      { "url" : "/media", "path" : "./media" },
+      { "url" : "/files/media", "path" : "./media" },
+      { "url" : "/static/", "path" : "./static_media" },
+      { "url" : "/files/static/", "path" : "./static_media" },
     ],
-  },
-  file_server: {
-    enable: true,
-    document_root: ".",
-    alias: [
-      { url: "/media", path: "./media" },
-      { url: "/files/media", path: "./media" },
-      { url: "/static/", path: "./static_media" },
-      { url: "/files/static/", path: "./static_media" },
-    ],
-  },
+  }
 }
--- config-scgi-example.js
+++ config-scgi-example.js
@@ -1,14 +1,14 @@
-{
-  application: {
-    // user specific data
-    connection_string: "mysql:database=oastat",
-    media_path: "../media",
-    static_media: "../static",
-  },
-
-  service: {
-    api: "scgi",
-    ip: "127.0.0.1",
-    port: 8081,
-  },
+{    
+  "application" : {
+    // user specific data  
+    "connection_string" : "mysql:database=oastat",
+    "media_path" : "../media",
+    "static_media" : "../static"
+  }
+,
+  "service" : {
+    "api" : "scgi",
+    "ip" : "127.0.0.1",
+    "port" : 8081
+  }
 }
